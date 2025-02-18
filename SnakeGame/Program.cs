39474eb1ddd9fec969279fa5c12f1b9e4c9e9a77
y_pachytskaya <y_pachytskaya@utb.cz>
2025-02-18 13:30:29 +0100

    - Oprava funkčnosti kodu

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -14,10 +14,10 @@ namespace Snake
         static string movement = "RIGHT";
         static List<int> snakeBodyX = new List<int>();
         static List<int> snakeBodyY = new List<int>();
-        static SnakeSegment snakeHead = new SnakeSegment();
+        static Pixel snakeHead = new Pixel();
         static int berryX, berryY;
 
-        static void Main(string[] args)
+        static void Main()
         {
             InitializeGame();
             GameLoop();
@@ -27,11 +27,10 @@ namespace Snake
         {
             Console.WindowHeight = screenHeight;
             Console.WindowWidth = screenWidth;
-            snakeHead.xpos = screenWidth / 2;
-            snakeHead.ypos = screenHeight / 2;
-            snakeHead.color = ConsoleColor.Red;
-            berryX = random.Next(0, screenWidth);
-            berryY = random.Next(0, screenHeight);
+            snakeHead.X = screenWidth / 2;
+            snakeHead.Y = screenHeight / 2;
+            snakeHead.Color = ConsoleColor.Red;
+            GenerateBerry();
         }
 
         static void GameLoop()
@@ -40,14 +39,14 @@ namespace Snake
             {
                 Console.Clear();
                 DrawBorders();
-                CheckCollision();
-                DrawSnakeAndBerry();
+                DrawSnake();
+                DrawBerry();
+                CheckCollisions();
                 HandleInput();
                 UpdateSnakePosition();
-                Thread.Sleep(100);
+                Thread.Sleep(500);
             }
-            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2);
-            Console.WriteLine("Game over, Score: " + score);
+            EndGame();
         }
 
         static void DrawBorders()
@@ -69,12 +68,20 @@ namespace Snake
             }
         }
 
-        static void DrawSnakeAndBerry()
+        static void DrawSnake()
         {
-            Console.ForegroundColor = snakeHead.color;
-            Console.SetCursorPosition(snakeHead.xpos, snakeHead.ypos);
+            Console.ForegroundColor = snakeHead.Color;
+            Console.SetCursorPosition(snakeHead.X, snakeHead.Y);
             Console.Write("■");
+            for (int i = 0; i < snakeBodyX.Count; i++)
+            {
+                Console.SetCursorPosition(snakeBodyX[i], snakeBodyY[i]);
+                Console.Write("■");
+            }
+        }
 
+        static void DrawBerry()
+        {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.SetCursorPosition(berryX, berryY);
             Console.Write("■");
@@ -84,24 +91,24 @@ namespace Snake
         {
             if (Console.KeyAvailable)
             {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.UpArrow && movement != "DOWN") movement = "UP";
-                if (key.Key == ConsoleKey.DownArrow && movement != "UP") movement = "DOWN";
-                if (key.Key == ConsoleKey.LeftArrow && movement != "RIGHT") movement = "LEFT";
-                if (key.Key == ConsoleKey.RightArrow && movement != "LEFT") movement = "RIGHT";
+                ConsoleKey key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.UpArrow && movement != "DOWN") movement = "UP";
+                if (key == ConsoleKey.DownArrow && movement != "UP") movement = "DOWN";
+                if (key == ConsoleKey.LeftArrow && movement != "RIGHT") movement = "LEFT";
+                if (key == ConsoleKey.RightArrow && movement != "LEFT") movement = "RIGHT";
             }
         }
 
         static void UpdateSnakePosition()
         {
-            snakeBodyX.Add(snakeHead.xpos);
-            snakeBodyY.Add(snakeHead.ypos);
+            snakeBodyX.Add(snakeHead.X);
+            snakeBodyY.Add(snakeHead.Y);
             switch (movement)
             {
-                case "UP": snakeHead.ypos--; break;
-                case "DOWN": snakeHead.ypos++; break;
-                case "LEFT": snakeHead.xpos--; break;
-                case "RIGHT": snakeHead.xpos++; break;
+                case "UP": snakeHead.Y--; break;
+                case "DOWN": snakeHead.Y++; break;
+                case "LEFT": snakeHead.X--; break;
+                case "RIGHT": snakeHead.X++; break;
             }
             if (snakeBodyX.Count > score)
             {
@@ -110,33 +117,44 @@ namespace Snake
             }
         }
 
-        static void CheckCollision()
+        static void CheckCollisions()
         {
-            if (snakeHead.xpos == screenWidth - 1 || snakeHead.xpos == 0 ||
-                snakeHead.ypos == screenHeight - 1 || snakeHead.ypos == 0)
+            if (snakeHead.X == 0 || snakeHead.X == screenWidth - 1 ||
+                snakeHead.Y == 0 || snakeHead.Y == screenHeight - 1)
             {
                 isGameOver = true;
             }
             for (int i = 0; i < snakeBodyX.Count; i++)
             {
-                if (snakeBodyX[i] == snakeHead.xpos && snakeBodyY[i] == snakeHead.ypos)
+                if (snakeBodyX[i] == snakeHead.X && snakeBodyY[i] == snakeHead.Y)
                 {
                     isGameOver = true;
                 }
             }
-            if (berryX == snakeHead.xpos && berryY == snakeHead.ypos)
+            if (snakeHead.X == berryX && snakeHead.Y == berryY)
             {
                 score++;
-                berryX = random.Next(1, screenWidth - 2);
-                berryY = random.Next(1, screenHeight - 2);
+                GenerateBerry();
             }
         }
 
-        class SnakeSegment
+        static void GenerateBerry()
         {
-            public int xpos { get; set; }
-            public int ypos { get; set; }
-            public ConsoleColor color { get; set; }
+            berryX = random.Next(1, screenWidth - 2);
+            berryY = random.Next(1, screenHeight - 2);
+        }
+
+        static void EndGame()
+        {
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2);
+            Console.WriteLine($"Game over, Score: {score}");
+        }
+
+        class Pixel
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public ConsoleColor Color { get; set; }
         }
     }
 }
